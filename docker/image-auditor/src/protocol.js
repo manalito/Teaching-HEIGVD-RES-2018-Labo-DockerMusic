f320c8b407b63bec04763e874e19667593cfa9c5
manalito <aurelsiu@hotmail.com>
2018-05-16 00:46:26 +0200

    working version

---
+++ docker/image-auditor/src/protocol.js
@@ -0,0 +1,17 @@
+/*
+ * Our application protocol specifies the following default multicast address and port
+ */
+
+exports.PROTOCOL_MULTICAST_ADDRESS = "239.255.22.5";
+exports.PROTOCOL_PORT = 9907;
+
+// Hier is specified the sounds of the instruments
+exports.PIANO = "ti-ta-ti";
+exports.TRUMPER = "pouet";
+exports.FLUTE = "trulu";
+exports.VIOLIN = "gzi-gzi";
+exports.DRUM = "boum-boum";
+
+exports.SOUND_INTERVAL = 1000;
+
+exports.DELAY_TCP = 5000;
